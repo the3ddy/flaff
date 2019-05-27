a18ad99c865d64af5afc49518d0998969589bffa
Edward Roh <edwardroh89@gmail.com>
2019-05-27 11:09:46 -0700

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, './src/client/index.js'),
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.tsx', '.ts'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/index.html",
+            filename: "./index.html"
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        historyApiFallback: true,
+        proxy: {
+            '/': 'http://localhost:3000',
+        }
+    }
+}
